6ae121158802077c8b394bce3ba76930fd4e45ff
sagnikdatta2k6 <sagnik.datta111@gmail.com>
2025-03-22 23:09:33 +0530

    Added TXT to PDF conversion

--- file_converter.py
+++ file_converter.py
@@ -9,3 +9,17 @@ try:
     POWERPOINT_INSTALLED = True
 except ImportError:
     POWERPOINT_INSTALLED = False
+
+
+# Convert Text file to PDF
+def convert_txt_to_pdf(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as file:
+        text = file.read()
+    
+    pdf = FPDF()
+    pdf.set_auto_page_break(auto=True, margin=15)
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.multi_cell(0, 10, text)
+    pdf.output(output_file)
+    print(f"Converted TXT to PDF: {output_file}")
